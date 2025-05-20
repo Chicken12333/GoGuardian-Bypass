6bf7ba088eebecaa75708c0548bcec58488fbec1
Chicken12333 <29240@d426.org>
2025-05-19 20:32:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function generateIframe() {
-    var url = document.getElementById('url').value;
+    var url = document.getElementById('https://blocked.goguardian.com/?ctx=YWZpPTExODE3NzkmYXBpPTc2ODc5Jm9pPTE2MTE5NzQxMzE5Jm91PWh0dHBzJTNBJTJGJTJGd3d3LmRldmlhbnRhcnQuY29tJTJGdGlsNjclMkZmYXZvdXJpdGVzJTJGOTMzOTU4NzUlMkZvbmRpbmUtZXQtZmxvcmEmcnM9QURNSU5fU0lURV9DQVRFR09SWV9GSUxURVImc2NpPTUmc3Q9Y2hyb21pdW0tbSZ2PTE%3D&sum=7962f5fa').value;
     var iframe = document.createElement('iframe');
     iframe.src = url;
     document.body.appendChild(iframe);
@@ -10,4 +10,4 @@ document.getElementById('url').addEventListener('keyup', function(event) {
         event.preventDefault();
         generateIframe();
     }
-});
+https://blocked.goguardian.com/?ctx=YWZpPTExODE3NzkmYXBpPTc2ODc5Jm9pPTE2MTE5NzQxMzE5Jm91PWh0dHBzJTNBJTJGJTJGd3d3LmRldmlhbnRhcnQuY29tJTJGdGlsNjclMkZmYXZvdXJpdGVzJTJGOTMzOTU4NzUlMkZvbmRpbmUtZXQtZmxvcmEmcnM9QURNSU5fU0lURV9DQVRFR09SWV9GSUxURVImc2NpPTUmc3Q9Y2hyb21pdW0tbSZ2PTE%3D&sum=7962f5fa
